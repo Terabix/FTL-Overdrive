2eb821a74d2006ed478fe0b72addcb6f5a12484b
DiEvAl <z-diomidov@yandex.ru>
2013-06-04 13:41:21 +0300

    Forgot to add the theme

--- src/com/ftloverdrive/MainMenu.java
+++ src/com/ftloverdrive/MainMenu.java
@@ -53,7 +53,7 @@ public class MainMenu extends State {
 				frame.setResizableAxis(ResizableAxis.BOTH);
 				frame.setDraggable(true);
 				
-				HTMLTextAreaModel tam = new HTMLTextAreaModel("<span style='font-family: bold;'>Lorem ipsum</span> dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+				HTMLTextAreaModel tam = new HTMLTextAreaModel("<span style='font-family: bold;'>Lorem ipsum</span> dolor sit amet<img src='door'></img>, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
 				TextArea ta = new TextArea(tam);
 				ta.setTheme("/textarea");
 				frame.add(ta);
